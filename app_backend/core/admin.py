d892117fb09926710e5f311b173fa186d2f8d395
savalan61 <savalandaglari2020@gmail.com>
2024-09-12 18:53:43 +0300

    api

--- app_backend/core/admin.py
+++ app_backend/core/admin.py
@@ -3,32 +3,26 @@ from .models import CategoryModel, BrandModel, ProductModel
 from django.utils.html import format_html
 
 class CategoryModelAdmin(admin.ModelAdmin):
-    # Display title and image in the table
-    list_display = ('title', 'image_tag')
+    list_display = ('id', 'title', 'image_tag')
     
     def image_tag(self, obj):
-        # Display image as HTML tag in the table
         if obj.imageUrl:
             return format_html('<img src="{}" width="100" height="100" />', obj.imageUrl)
         return '-'
-    image_tag.short_description = 'Image'  # Change column title to 'Image'
+    image_tag.short_description = 'Image'
 
 class BrandModelAdmin(admin.ModelAdmin):
-    # Display title and image in the table
-    list_display = ('title', 'image_tag')
+    list_display = ('id', 'title', 'image_tag')
     
     def image_tag(self, obj):
-        # Display image as HTML tag in the table
         if obj.imageUrl:
             return format_html('<img src="{}" width="100" height="100" />', obj.imageUrl)
         return '-'
-    image_tag.short_description = 'Image'  # Change column title to 'Image'
+    image_tag.short_description = 'Image'
 
 class ProductModelAdmin(admin.ModelAdmin):
-    # Display title, price, rating, category, and brand in the table
-    list_display = ('title', 'price', 'rating', 'category', 'brand')
+    list_display = ('id', 'title', 'price', 'rating', 'category', 'brand')
 
-# Register models with custom settings
 admin.site.register(CategoryModel, CategoryModelAdmin)
 admin.site.register(BrandModel, BrandModelAdmin)
 admin.site.register(ProductModel, ProductModelAdmin)
