2b34af002168cd2f017b5b19f615bcf81a0c6355
savalan61 <savalandaglari2020@gmail.com>
2024-09-18 01:21:33 +0300

    n

--- app_backend/wishlist/admin.py
+++ app_backend/wishlist/admin.py
@@ -1,12 +1,19 @@
+# wishlist/admin.py
+
 from django.contrib import admin
+from .models import UserWishlist
 
-# Register your models here.
-from django.contrib import admin
-from . models import  UserWishlist
+class UserWishlistAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'user_id', 'favorite_products_list', 'created_at')
+    search_fields = ('user__username',)  # امکان جستجو بر اساس نام کاربری
+    list_filter = ('created_at',)  # امکان فیلتر بر اساس تاریخ
 
+    def user_id(self, obj):
+        return obj.user.id
+    user_id.short_description = 'User ID'  # نام ستون در پنل مدیریت
 
+    def favorite_products_list(self, obj):
+        return ", ".join([str(product) for product in obj.favorite_products.all()])
+    favorite_products_list.short_description = 'Favorite Products'  # نام ستون در پنل مدیریت
 
-# @admin.register(UserWishlist)
-# class UserWishlistAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'created_at')
-admin.site.register(UserWishlist)
+admin.site.register(UserWishlist, UserWishlistAdmin)
--- app_backend/wishlist/views.py
+++ app_backend/wishlist/views.py
@@ -1,22 +1,17 @@
-from rest_framework import status, generics
+# views.py
+
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from . import models, serializers
-
-class GetWishList(generics.ListAPIView):
-    serializer_class = serializers.WishListSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return models.UserWishlist.objects.filter(user=self.request.user)
+from . import models
 
 class ToggleWishes(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    def post(self, request):
         user = request.user
-        product_id = request.query_params.get('id')
+        product_id = request.data.get('id')  # استفاده از request.data برای POST
 
         if not product_id:
             return Response({'message': 'Product id is required'}, status=status.HTTP_400_BAD_REQUEST)
@@ -32,7 +27,7 @@ class ToggleWishes(APIView):
         # Add or remove the product from the wish list
         if product in wish_list.favorite_products.all():
             wish_list.favorite_products.remove(product)
-            return Response({'message': 'Product removed from wish list'}, status=status.HTTP_204_NO_CONTENT)
+            return Response({'message': 'Product removed from wish list'}, status=status.HTTP_200_OK)
         else:
             wish_list.favorite_products.add(product)
-            return Response({'message': 'Product added to wish list'}, status=status.HTTP_201_CREATED)
+            return Response({'message': 'Product added to wish list'}, status=status.HTTP_200_OK)
